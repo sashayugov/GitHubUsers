1a2cb6f62f1e97482411268534341c3e59ad6e01
Yugov Aleksandr <sashayugov@gmail.com>
2023-02-22 16:04:52 +0400

    add rx implementations

--- app/src/main/java/com/example/githubusers/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubusers/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.githubusers
+package com.example.githubusers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import com.example.githubusers.R
 import com.example.githubusers.databinding.ActivityMainBinding
 import com.example.githubusers.ui.userslist.UsersListFragment
 
